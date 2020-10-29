796ad5ee63add90c9b71bde27c37e672ed2dc856
shady2498 <shahhassancs@gmail.com>
2020-10-29 12:52:42 +0500

    removed errors in signup and sign in

--- components/SignIn.js
+++ components/SignIn.js
@@ -3,57 +3,38 @@ import useForm from "../customhooks/useForm";
 
 export default function SignIn() {
 	const { handleChange, values } = useForm();
+	// console.log(values);
 
-	const signInInfo = (event, values) => {
+	const saveInfo = (event, values) => {
 		event.preventDefault();
 		var myJSON = JSON.stringify(values);
 		console.log(myJSON);
 	};
+
 	return (
-		<form onSubmit={(event) => signInInfo(event, values)}>
-			<h1>Name</h1>
-			<label htmlFor="name">
-				<input
-					type="name"
-					name="name"
-					placeholder="Name"
-					value={values.name}
-					onChange={handleChange}
-				></input>
-			</label>
-			<h1>Email</h1>
+		<form onSubmit={(event) => saveInfo(event, values)}>
+			<h1>email</h1>
 			<label htmlFor="email">
 				<input
 					type="email"
 					name="email"
-					placeholder="email"
+					placeholder="Email"
 					value={values.email}
 					onChange={handleChange}
 				></input>
 			</label>
-
 			<h1>Password</h1>
 			<label htmlFor="password">
 				<input
 					type="password"
 					name="password"
-					placeholder="password"
+					placeholder="Password"
 					value={values.password}
 					onChange={handleChange}
 				></input>
 			</label>
-			<h1>Pin Code</h1>
-			<label htmlFor="pincode">
-				<input
-					type="pincode"
-					name="pincode"
-					placeholder="pincode"
-					value={values.pincode}
-					onChange={handleChange}
-				></input>
-			</label>
 
-			<button type="submit">Signin</button>
+			<button type="submit">Signup</button>
 		</form>
 	);
 }
